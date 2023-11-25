5387c1a49df7f01dabc7a1138eb00b41c0033ba9
Yosef Alemu <yosefalemumengstie@gmail.com>
2023-11-25 15:22:50 +0300

    backend intial setup done

---
+++ Backend/index.js
@@ -0,0 +1,30 @@
+require("dotenv").config();
+require("express-async-errors");
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const connectDB = require("./database/connect");
+const errorHandlerMiddleware = require("./middlewares/error-handler");
+const notFoundMiddleWare = require("./middlewares/not-found");
+const authMiddleWare = require("./middlewares/authenticate");
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use("/uploads", express.static(__dirname + "/public/assets"));
+
+const userRouter = require("./routes/userRoute");
+
+app.use("/api/v1/user", userRouter);
+
+app.use(notFoundMiddleWare);
+app.use(errorHandlerMiddleware);
+
+const port = process.env.PORT || 3001;
+const start = async () => {
+  await connectDB(process.env.MONGO_URL);
+  app.listen(port, () => console.log(`server is listening on port ${port}`));
+};
+
+start();
---
+++ Backend/middlewares/authenticate.js
@@ -0,0 +1,20 @@
+const jwt = require("jsonwebtoken");
+const { UnauthenticatedError } = require("../errors");
+
+const authMiddleWare = async (req, res, next) => {
+  const authHeader = req.headers.authorization;
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    throw new UnauthenticatedError("authentication invalid");
+  }
+  try {
+    const token = authHeader.split(" ")[1];
+    const payload = jwt.verify(token, process.env.JWT_SECRET);
+    // req.user = { userId: payload.userId, name: payload.name };
+    // req.user = await UserSchema.findById(payload.userId).select("-password");
+    next();
+  } catch (error) {
+    throw new UnauthenticatedError("authentication invalid");
+  }
+};
+
+module.exports = authMiddleWare;
