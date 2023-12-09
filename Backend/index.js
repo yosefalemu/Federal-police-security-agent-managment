7867f903d7845561c08137735d31cb1c288f8cd1
Misgan Moges <misganmoges03@gmail.com>
2023-12-10 01:12:51 +0300

    env file updated

--- Backend/index.js
+++ Backend/index.js
@@ -1,30 +1,64 @@
 require("dotenv").config();
 require("express-async-errors");
+// express
+
 const express = require("express");
-const cors = require("cors");
-const bodyParser = require("body-parser");
-const connectDB = require("./database/connect");
-const errorHandlerMiddleware = require("./middlewares/error-handler");
-const notFoundMiddleWare = require("./middlewares/not-found");
-const authMiddleWare = require("./middlewares/authenticate");
-
 const app = express();
+// rest of the packages
+const morgan = require("morgan");
+const cookieParser = require("cookie-parser");
+const fileUpload = require("express-fileupload");
+const rateLimiter = require("express-rate-limit");
+const helmet = require("helmet");
+const xss = require("xss-clean");
+const cors = require("cors");
+const mongoSanitize = require("express-mongo-sanitize");
+
+// database
+const connectDB = require("./db/connect");
+
+//  routers
+const authRouter = require("./routes/authRoutes");
+const userRouter = require("./routes/userRoutes");
+
+// middleware
+const notFoundMiddleware = require("./middleware/not-found");
+const errorHandlerMiddleware = require("./middleware/error-handler");
+
+app.set("trust proxy", 1);
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000,
+    max: 60,
+  })
+);
+app.use(helmet());
 app.use(cors());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use("/uploads", express.static(__dirname + "/public/assets"));
+app.use(xss());
+app.use(mongoSanitize());
 
-const userRouter = require("./routes/userRoute");
+app.use(express.json());
+app.use(cookieParser(process.env.JWT_SECRET));
 
-app.use("/api/v1/user", userRouter);
+app.use(express.static("./public"));
+app.use(fileUpload());
 
-app.use(notFoundMiddleWare);
+app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/users", userRouter);
+
+app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
 
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 5000;
 const start = async () => {
-  await connectDB(process.env.MONGO_URL);
-  app.listen(port, () => console.log(`server is listening on port ${port}`));
+  try {
+    await connectDB(process.env.MONGO_URL);
+    app.listen(port, () =>
+      console.log(`Server is listening on port ${port}...`)
+    );
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 start();
